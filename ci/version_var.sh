da35e2fabae4374a4b685f332d704661be688641
Troy Ronda <troy.ronda@securekey.com>
2020-05-08 13:38:23 -0400

    ci: publish snapshot images to snapshot repo
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

--- ci/version_var.sh
+++ ci/version_var.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Copyright SecureKey Technologies Inc. All Rights Reserved.
 #
@@ -9,12 +10,22 @@
 BASE_VERSION=0.1.4
 IS_RELEASE=false
 
-if [ $IS_RELEASE == false ]
+# Project Parameters
+SOURCE_REPO=trustbloc-did-method
+BASE_PKG_NAME=driver-did-trustbloc
+RELEASE_REPO=docker.pkg.github.com/trustbloc/${SOURCE_REPO}
+SNAPSHOT_REPO=docker.pkg.github.com/trustbloc-cicd/snapshot
+
+if [ ${IS_RELEASE} = false ]
 then
   EXTRA_VERSION=snapshot-$(git rev-parse --short=7 HEAD)
-  PROJECT_VERSION=$BASE_VERSION-$EXTRA_VERSION
+  PROJECT_VERSION=${BASE_VERSION}-${EXTRA_VERSION}
+  PROJECT_PKG_REPO=${SNAPSHOT_REPO}
 else
-  PROJECT_VERSION=$BASE_VERSION
+  PROJECT_VERSION=${BASE_VERSION}
+  PROJECT_PKG_REPO=${RELEASE_REPO}
 fi
 
-export BLOC_DID_METHOD_TAG=$PROJECT_VERSION
+
+export TRUSTBLOC_DID_METHOD_TAG=${PROJECT_VERSION}
+export TRUSTBLOC_DID_METHOD_PKG=${PROJECT_PKG_REPO}/${BASE_PKG_NAME}
